e95404efbffa83b4971246ad4540d7af5989e242
Oleg Nagaitsev <onagaitsev@yandex.ru>
2020-09-07 21:20:14 +0300

    Code corrected

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,28 +1,50 @@
 fun main() {
-    val likes = 10555
+    val likes = 126742
+    val neOnes = likes / 10
     val ones = likes % (likes / 10)
-    var ending: String = "";
+    val tens = likes / 10 % 10 * 10 + ones
+    var ending: String = ""
 
-    when (ones) {
-        0 -> ending = "подписчиков"
-        1 -> ending = "подписчик"
-        2 -> ending = "подписчика"
-        3 -> ending = "подписчика"
-        4 -> ending = "подписчика"
-        5 -> ending = "подписчиков"
-        6 -> ending = "подписчиков"
-        7 -> ending = "подписчиков"
-        8 -> ending = "подписчиков"
-        9 -> ending = "подписчиков"
+    if ((likes >= 0) && (likes < 10)) {
+        when (likes) {
+            0 -> ending = "подписчиков"
+            1 -> ending = "подписчик"
+            2 -> ending = "подписчика"
+            3 -> ending = "подписчика"
+            4 -> ending = "подписчика"
+            5 -> ending = "подписчиков"
+            6 -> ending = "подписчиков"
+            7 -> ending = "подписчиков"
+            8 -> ending = "подписчиков"
+            9 -> ending = "подписчиков"
+        }
+    } else {
+        if ((likes >= 10) && (likes <= 20)) {
+            ending = "подписчиков"
+        } else {
+            if ((tens > 10) && (tens < 20)) {
+                ending = "подписчиков"
+            } else {
+
+                if ((likes > 20) || (likes < 10)) {
+                    when (ones) {
+                        0 -> ending = "подписчиков"
+                        1 -> ending = "подписчик"
+                        2 -> ending = "подписчика"
+                        3 -> ending = "подписчика"
+                        4 -> ending = "подписчика"
+                        5 -> ending = "подписчиков"
+                        6 -> ending = "подписчиков"
+                        7 -> ending = "подписчиков"
+                        8 -> ending = "подписчиков"
+                        9 -> ending = "подписчиков"
+                    }
+                }
+            }
+        }
     }
-        println("У Вас $likes $ending")
-
-
-
-
-
-
-
-
-
+    println("neOnes = $neOnes")
+    println("ones = $ones")
+    println("tens = $tens")
+    println("У Вас $likes $ending")
 }
