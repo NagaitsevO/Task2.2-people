257ef96a0b40dcaa300ef5e43806494f6fa81420
Oleg Nagaitsev <onagaitsev@yandex.ru>
2020-09-09 19:24:58 +0300

    Some temp commands deleted

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 126742
+    val likes = 126714
     val ones = likes % (likes / 10)
     val tens = likes / 10 % 10 * 10 + ones
     var ending: String = ""
@@ -24,7 +24,6 @@ fun main() {
             if ((tens > 10) && (tens < 20)) {
                 ending = "подписчиков"
             } else {
-
                 if ((likes > 20) || (likes < 10)) {
                     when (ones) {
                         0 -> ending = "подписчиков"
@@ -42,8 +41,5 @@ fun main() {
             }
         }
     }
-    println("neOnes = $neOnes")
-    println("ones = $ones")
-    println("tens = $tens")
     println("У Вас $likes $ending")
 }
